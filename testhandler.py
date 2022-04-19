fb448d9925c1fed3a2b594701cf5890888b872db
estronnom <estronnom@gmail.com>
2022-04-20 00:58:34 +0300

    fix

--- testhandler.py
+++ testhandler.py
@@ -11,7 +11,7 @@ bot = AsyncTeleBot(constants.APIKEY, parse_mode=None)
 db = dbHandler(constants.DBPARAMS)
 apihelper.SESSION_TIME_TO_LIVE = 300
 stack = {}
-rolemapping = {'user':'пользователь','admin':'администратор','owner':'владелец'}
+rolemapping = {'user':'пользователь','owner':'владелец'}
 
 def stackFilter(message, term, mustbedigit = False):
     if mustbedigit and not message.text.isdigit:
@@ -23,38 +23,60 @@ def stackFilter(message, term, mustbedigit = False):
 def clearUserStack(id):
     stack[id] = {}
 
-async def csvLoadSender(idUser, loadSource, callData):
+@bot.message_handler(commands=['office'])
+async def messageHandler(message):
+    getRole = db.ex('SELECT role FROM employee WHERE chatid = %s', (int(message.chat.id),))
+    if not getRole:
+        await bot.send_message(message.chat.id, 'Такой пользователь не зарегистрирован в системе\nОбратитесь к администратору\n/authuser для авторизации')
+    elif getRole[0][0] == 'owner':
+        await bot.send_message(message.chat.id, "Личный кабинет администратора", reply_markup=mk.createMarkup(2, ['Автомобили', 'Сотрудники', 'Затраты', 'Доходы', 'Сводка', 'Выдать З/П'], ['adAuto', 'adEmployee', 'adExpenses', 'adIncome', 'adPivot', 'adWage']))
+    elif getRole[0][0] == 'user':
+        await bot.send_message(message.chat.id, 'Личный кабинет сотрудника', reply_markup=mk.createMarkup(1,['Отчитаться о поездке','Мои доходы'],['userTask','userIncome']))
+    else:
+        await bot.send_message(message.chat.id, 'Проблемы с ролью пользователя\nОбратитесь к администратору')
+    clearUserStack(message.chat.id)
+
+async def csvLoadSender(idUser, expense, callData):
     try:
         period = int(callData[12:])
-        load = db.ex(f"SELECT date_trunc('minute', timedate), employee.name, amount, info FROM {loadSource} JOIN employee ON employee.id = person WHERE timedate >= NOW()::date - INTEGER '%s' ", (period,))
     except ValueError:
-        load = db.ex(f"SELECT date_trunc('minute', timedate), employee.name, amount, info FROM {loadSource} JOIN employee ON employee.id = person")
+        period = ''
+    else:
+        period = f" WHERE time >= NOW()::date - INTEGER '{period}'"
+    if expense:
+        load = db.ex("SELECT date_trunc('minute', time), task.object, COALESCE(employee.name, employee.handle), amount, note FROM expenses JOIN task ON taskid = task.id JOIN employee ON employeeid = employee.id" + period)
+        headers = 'Время,Объект,Сотрудник,Сумма,Заметка\n'
+    else:
+        load = db.ex("SELECT date_trunc('minute', time), task.object, COALESCE(employee.name, employee.handle), income FROM employeetotask JOIN employee ON employeeid = employee.id JOIN task ON taskid = task.id WHERE main = true" + period)
+        headers = 'Время,Объект,Сотрудник,Сумма\n'
     if not load:
         await bot.send_message(idUser, 'Записей за данный период не найдено')
     else:
         load = '\n'.join([','.join([str(obj) for obj in item]) for item in load])
-        f = io.StringIO('Время,Сотрудник,Сумма,Информация\n'+load)
-        await bot.send_document(idUser, f, visible_file_name=f'{loadSource}.csv')
+        fh = io.StringIO(headers + load)
+        await bot.send_document(idUser, fh, visible_file_name = f'{"Expenses" if expense else "Income"}.csv')
 
 async def insertDigit(message, direction, note, func):
     try:
         digit = func(message.text.replace(',' , '.'))
         stack[message.chat.id][direction] = digit
         print(f'{direction} is {digit}')
-        await bot.send_message(message.chat.id, note)
+        if note:
+            await bot.send_message(message.chat.id, note)
     except ValueError:
         await bot.send_message(message.chat.id, 'Число введено некорректо, попробуйте еще раз')
 
 @bot.callback_query_handler(func=lambda call: True)
 async def callbackQuery(call):
     clearUserStack(call.from_user.id)
-    if call.data.startswith('auth_'):
-        data = call.data.split('_')
-        db.ex('INSERT INTO users(handle, chatid, role) VALUES (%s, %s, %s) ON CONFLICT (chatid) DO UPDATE SET role = %s WHERE users.chatid = %s', (data[1], int(data[2]), data[3], data[3], int(data[2])))
+    if call.data.startswith('auth'):
+        data = call.data.split('//')
+        db.ex('INSERT INTO employee(handle, chatid, role) VALUES (%s, %s, %s) ON CONFLICT (chatid) DO UPDATE SET role = %s WHERE employee.chatid = %s', (data[1], int(data[2]), data[3], data[3], int(data[2])))
         await bot.send_message(data[2], f'Ваши привилегии обновлены\nНовая роль: {rolemapping[data[3]]}\n/office для перехода в личный кабинет')
         await bot.send_message(data[-1], 'Роль успешно обновлена')
     elif call.data == "adAuto":
-        await bot.send_message(call.from_user.id, 'Список авто:\n' + getAutoList(), reply_markup=mk.createMarkup(2, ['Добавить', 'Удалить'], ['adAutoAdd', 'adAutoDelete']))
+        autoList = await getAutoList()
+        await bot.send_message(call.from_user.id, 'Список авто:\n' + autoList, reply_markup=mk.createMarkup(2, ['Добавить', 'Удалить'], ['adAutoAdd', 'adAutoDelete']))
     elif call.data == "adAutoAdd":
         stack[call.from_user.id]['addingAuto'] = True
         stack[call.from_user.id]['addingAutoName'] = None
@@ -70,15 +92,18 @@ async def callbackQuery(call):
         stack[call.from_user.id]['deletingAuto'] = False
         await bot.send_message(call.from_user.id, 'Удаление автомобилей закончено\n/office для входа в личный кабинет')
     elif call.data == "adEmployee":
-        await bot.send_message(call.from_user.id, 'Список сотрудников:\n' + getEmployees(coeff = True), reply_markup=mk.createMarkup(2, ['Добавить', 'Удалить'], ['adEmployeeAdd', 'adEmployeeDelete']))
-    elif call.data == "adEmployeeAdd":
-        await bot.send_message(call.from_user.id, 'Добавляем сотрудника в базу\nВведите имя')
-        stack[call.from_user.id]['addingEmployee'] = True
-        stack[call.from_user.id]['addingEmployeeName'] = None
-        stack[call.from_user.id]['addingEmployeeCoeff'] = None
-    elif call.data == "endEmployeeAdd":
+        employeeList = await getEmployees(coeff = True)
+        await bot.send_message(call.from_user.id, 'Список сотрудников:\n' + employeeList, reply_markup=mk.createMarkup(1, ['Обновить информацию', 'Удалить'], ['adEmployeeUpdate', 'adEmployeeDelete']))
+    elif call.data == "adEmployeeUpdate":
+        await bot.send_message(call.from_user.id, 'Сотрудник есть в системе, однако ему нужно назначить имя, зарплату и коэффициент\nВведите номер сотрудника, информацию о котором собираетесь редактировать')
+        stack[call.from_user.id]['updateEmployee'] = True
+        stack[call.from_user.id]['updateEmployeeId'] = None
+        stack[call.from_user.id]['updateEmployeeName'] = None
+        stack[call.from_user.id]['updateEmployeeCoeff'] = None
+        stack[call.from_user.id]['updateEmployeeWage'] = None
+    elif call.data == "endEmployeeUpdate":
         stack[call.from_user.id]['addingEmployee'] = False
-        await bot.send_message(call.from_user.id, 'Добавление закончено\n/office для входа в личный кабинет')
+        await bot.send_message(call.from_user.id, 'Обновление закончено\n/office для входа в личный кабинет')
     elif call.data == "adEmployeeDelete":
         await bot.send_message(call.from_user.id, 'Введите номер сотрудника, которого желаете удалить из базы')
         stack[call.from_user.id]['deletingEmployee'] = True
@@ -88,11 +113,22 @@ async def callbackQuery(call):
     elif call.data == "adExpenses":
         await bot.send_message(call.from_user.id, 'Выберите период выгрузки расходов', reply_markup=mk.createMarkup(2, ['24 часа', 'Неделя', 'Месяц', 'Все время'], ['loadExpenses1', 'loadExpenses7', 'loadExpenses30', 'loadExpensesAll']))
     elif call.data.startswith('loadExpenses'):
-        await csvLoadSender(call.from_user.id, 'expenses', call.data)
+        await csvLoadSender(call.from_user.id, True, call.data)
     elif call.data == "adIncome":
         await bot.send_message(call.from_user.id, 'Выберите период выгрузки доходов', reply_markup=mk.createMarkup(2, ['24 часа', 'Неделя', 'Месяц', 'Все время'], ['loadIncome1', 'loadIncome7', 'loadIncome30', 'loadIncomeAll']))
     elif call.data.startswith('loadIncome'):
-        await csvLoadSender(call.from_user.id, 'incomes', call.data)
+        await csvLoadSender(call.from_user.id, False, call.data)
+    #сводка по расходам и доходам
+    #elif call.data == 'adPivot':
+    #    await bot.send_message(call.from_user.id, 'Выберите период выгрузки расходов', reply_markup=mk.createMarkup(2, ['24 часа', 'Неделя', 'Месяц', 'Все время'], ['loadPivot1', 'loadPivot7', 'loadPivot30', 'loadPivotAll']))
+    elif call.data == 'adWage':
+        await bot.send_message(call.from_user.id, 'Администратору предстоит подтвердить все совершенные поездки и начислить за них заработную плату сотрдуникам', reply_markup=mk.createMarkup(1, ['Продолжить?'],['adWageStart']))
+    elif call.data.startswith('adWageStart'):
+        if call.data != 'adWageStart':
+            pass
+            #insert
+        print(db.ex('SELECT * FROM employeetotask JOIN employee ON employeeid = employee.id JOIN task ON taskid = task.id WHERE paid IS NOT true'))
+        #SELECT name, object, hoursspent, hoursspent * wage * coeff AS sum FROM employeetotask JOIN employee ON employeeid = employee.id JOIN task ON taskid = task.id WHERE paid IS NOT true;
     elif call.data == "userTask":
         stack[call.from_user.id]['creatingTask'] = True
         stack[call.from_user.id]['taskObject'] = None
@@ -101,7 +137,8 @@ async def callbackQuery(call):
         stack[call.from_user.id]['taskKm'] = None
         stack[call.from_user.id]['taskTime'] = None
         stack[call.from_user.id]['taskIncome'] = None
-        stack[call.from_user.id]['taskExpenses'] = None
+        stack[call.from_user.id]['taskExpenses'] = []
+        stack[call.from_user.id]['taskExpensesFinished'] = False
         lastObject = db.ex('SELECT id, object FROM task ORDER BY time DESC LIMIT 10')
         if not lastObject:
             queryText = 'Заполняем задачу\nДля начала введите название объекта:'
@@ -157,9 +194,32 @@ async def createTask(message):
     elif not stack[message.chat.id]['taskTime']:
         await insertDigit(message, 'taskTime', 'Теперь введите сумму, полученную за заказ', int)
     elif not stack[message.chat.id]['taskIncome']:
-        await insertDigit(message, 'taskIncome', 'Теперь расходы', float)
+        await insertDigit(message, 'taskIncome', 'Теперь нужно отчитаться о связанных с поездкой расходах\nКаждую статью расходов нужно описать отдельным сообщением в следующей форме: \n\n1500 на бензин\n300 на мобильную связь\n\nКогда вы укажете все расходы(или если они отсутствуют), отправьте команду /skip', float)
+    elif not stack[message.chat.id]['taskExpensesFinished']:
+        if message.text == '/skip':
+            stack[message.chat.id]['taskExpensesFinished'] = True
+            idTask = db.ex('INSERT INTO task(object, car, kmspent, hoursspent, income, time) VALUES (%s, %s, %s, %s, %s, NOW()) RETURNING *', (stack[message.chat.id]['taskObject'], stack[message.chat.id]['taskCar'], stack[message.chat.id]['taskKm'], stack[message.chat.id]['taskTime'], stack[message.chat.id]['taskIncome']))[0][0]
+            idTaskMaker = db.ex('SELECT id FROM employee WHERE chatid = %s', (message.chat.id,))[0][0]
+            db.ex('INSERT INTO employeetotask(employeeid, taskid, main) VALUES(%s, %s, True)', (idTaskMaker, idTask))
+            if stack[message.chat.id]['taskBuddy']:
+                db.ex('INSERT INTO employeetotask(employeeid, taskid, main) VALUES (%s, %s, False) ON CONFLICT DO NOTHING', (stack[message.chat.id]['taskBuddy'], idTask))
+            if stack[message.chat.id]['taskExpenses']:
+                for expense in stack[message.chat.id]['taskExpenses']:
+                    db.ex('INSERT INTO expenses(taskid, employeeid, amount, note) VALUES (%s, %s, %s, %s)', (idTask, idTaskMaker, expense[0], expense[1]))
+            await bot.send_message(message.chat.id, 'Готово! Отчет успешно внесен в базу\n/office для перехода в личный кабинет')
+            clearUserStack(message.chat.id)
+        else:
+            expense = message.text.split()
+            try:
+                value = int(expense[0])
+            except ValueError:
+                await bot.send_message(message.chat.id, 'Сумма расхода введена некорректно\nВерный формат: 1000 на бензин')
+            else:
+                if expense and len(expense) > 1:
+                    stack[message.chat.id]['taskExpenses'].append((value, ' '.join(expense[1:])))
+                    print(stack[message.chat.id]['taskExpenses'])
     else:
-        clearUserStack(message.chat.id)
+        await clearUserStack(message.chat.id)
         await bot.send_message(message.chat.id, 'С отчетом о поездке произошла ошибка :(\nПерейдите в личный кабинет /office и попробуйте еще раз')
 
 async def getAutoList():
@@ -188,30 +248,36 @@ async def autoDeleter(message):
     await bot.send_message(message.chat.id, f'Машина {message.text} удалена из базы', reply_markup=mk.createMarkup(2, ['Удалить еще', 'Закончить'], ['adAutoDelete', 'endAutoDelete']))
 
 async def getEmployees(coeff):
-    employeeList = db.ex(f'SELECT id, name{", coeff" if coeff else ""} FROM employee WHERE deleted IS NOT TRUE')
+    employeeList = db.ex(f'SELECT id, name{", coeff, wage" if coeff else ""} FROM employee WHERE deleted IS NOT TRUE AND name IS NOT NULL')
     if employeeList:
         employeeList = '\n'.join([' '.join([str(obj) for obj in item]) for item in employeeList])
     else:
         employeeList = 'Список сотрудников пуст'
-    return employeeList
-
-@bot.message_handler(func=lambda message: stackFilter(message, 'addingEmployee'))
-async def addingEmployee(message):
-    if not stack[message.chat.id]['addingEmployeeName']:
-        stack[message.chat.id]['addingEmployeeName'] = message.text
-        await bot.send_message(message.chat.id, 'Теперь введите коэффициент зарплаты для сотрудника (введите 1, чтобы оставить сотрудника без коэффициента)')
+    unnamedEmployee = db.ex('SELECT id, handle FROM employee WHERE deleted IS NOT TRUE AND name IS NULL')
+    if unnamedEmployee:
+        unnamedEmployee = '\n'.join([' '.join([str(obj) for obj in item]) for item in unnamedEmployee])
+        unnamedEmployee = '\nВ базе также есть несколько сотрудников с неподтвержденным именем:\n' + unnamedEmployee
     else:
-        try:
-            coeff = float(message.text)
-        except ValueError:
-            await bot.send_message(message.chat.id, 'Число введено неверно, попробуйте еще раз\nДробное число нужно вводить с точкой: 1.5')
-            return
-        db.ex('INSERT INTO employee(name, coeff) VALUES (%s, %s)', param=(stack[message.chat.id]['addingEmployeeName'], message.text))
-        await bot.send_message(message.chat.id, 'Готово! Сотрудник добавлен в базу', reply_markup=mk.createMarkup(2, ['Добавить еще', 'Закончить'], ['adEmployeeAdd', 'endEmployeeAdd']))
+        unnamedEmployee = ''
+    return employeeList + unnamedEmployee
+
+@bot.message_handler(func=lambda message: stackFilter(message, 'updateEmployee'))
+async def addingEmployee(message):
+    if not stack[message.chat.id]['updateEmployeeId']:
+        await insertDigit(message, 'updateEmployeeId', 'Теперь введите имя для сотрудника', int)
+    elif not stack[message.chat.id]['updateEmployeeName']:
+        stack[message.chat.id]['updateEmployeeName'] = message.text
+        await bot.send_message(message.chat.id, 'Теперь введите коэффициент зарплаты для сотрудника (введите 1, чтобы оставить сотрудника без коэффициента)')
+    elif not stack[message.chat.id]['updateEmployeeCoeff']:
+        await insertDigit(message, 'updateEmployeeCoeff', 'Отлично, теперь введите часовую ставку сотрудника', float)
+    elif not stack[message.chat.id]['updateEmployeeWage']:
+        await insertDigit(message, 'updateEmployeeWage', None, int)
+        db.ex('UPDATE employee SET name = %s, coeff = %s, wage = %s WHERE id = %s', (stack[message.chat.id]['updateEmployeeName'], stack[message.chat.id]['updateEmployeeCoeff'], stack[message.chat.id]['updateEmployeeWage'], stack[message.chat.id]['updateEmployeeId']))
+        await bot.send_message(message.chat.id, 'Готово! Информация о сотруднике отредактирована', reply_markup=mk.createMarkup(2, ['Обновить еще', 'Закончить'], ['adEmployeeUpdate', 'endEmployeeUpdate']))
 
 @bot.message_handler(func=lambda message: stackFilter(message, 'deletingEmployee', mustbedigit = True))
 async def deletingEmployee(message):
-    db.ex('UPDATE employee SET deleted = True WHERE id = %s', param=(int(message.text),))
+    db.ex('UPDATE employee SET deleted = True WHERE id = %s', param=(message.text,))
     await bot.send_message(message.chat.id, f'Сотрудник {message.text} удален из базы', reply_markup=mk.createMarkup(2, ['Удалить еще', 'Закончить'], ['adEmployeeDelete', 'endEmployeeDelete']))
 
 @bot.message_handler(commands=['dbtest'])
@@ -221,22 +287,8 @@ async def testHandler(message):
 @bot.message_handler(commands=['authuser', 'authowner'])
 async def auth(message):
     role = message.text[5:]
-    for owner in db.ex("SELECT chatid FROM users WHERE role = 'owner'"):
-        await bot.send_message(owner[0], f'Новый запрос на авторизацию пользователя от @{message.chat.username}\nЗапрашиваемая роль: {rolemapping[role]}', reply_markup=mk.createMarkup(1, ['Авторизовать'],[f'auth_{message.chat.username}_{message.chat.id}_{role}_{owner[0]}']))
-
-@bot.message_handler(commands=['office'])
-async def messageHandler(message):
-    getRole = db.ex('SELECT role FROM users WHERE chatid = %s', (int(message.chat.id),))
-    if not getRole:
-        await bot.send_message(message.chat.id, 'Такой пользователь не зарегистрирован в системе\nОбратитесь к администратору\n/authuser для авторизации')
-    elif getRole[0][0] == 'owner':
-        await bot.send_message(message.chat.id, "Личный кабинет администратора", reply_markup=mk.createMarkup(2, ['Автомобили', 'Сотрудники', 'Затраты', 'Доходы'], ['adAuto', 'adEmployee', 'adExpenses', 'adIncome']))
-    elif getRole[0][0] == 'user':
-        await bot.send_message(message.chat.id, 'Личный кабинет сотрудника', reply_markup=mk.createMarkup(1,['Отчитаться о поездке','Мои доходы'],['userTask','userIncome']))
-    else:
-        await bot.send_message(message.chat.id, 'Проблемы с ролью пользователя\nОбратитесь к администратору')
-    clearUserStack(message.chat.id)
-
+    for owner in db.ex("SELECT chatid FROM employee WHERE role = 'owner'"):
+        await bot.send_message(owner[0], f'Новый запрос на авторизацию пользователя от @{message.chat.username}\nЗапрашиваемая роль: {rolemapping[role]}', reply_markup=mk.createMarkup(1, ['Авторизовать'],[f'auth//{message.chat.username}//{message.chat.id}//{role}//{owner[0]}']))
 
 def main():
     asyncio.run(bot.polling())

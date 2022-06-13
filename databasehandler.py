2873ec71fef7584155b6bc031db0c6c405c046ba
estronnom <estronnom@gmail.com>
2022-06-14 00:03:46 +0300

    db interface edited

--- databasehandler.py
+++ databasehandler.py
@@ -1,8 +1,10 @@
 import psycopg2
+import psycopg2.errors
 
 
-class dbHandler():
+class DatabaseHandler:
     def __init__(self, params):
+        self.params = params
         self.conn = psycopg2.connect(**params)
         self.cur = self.conn.cursor()
 
@@ -11,18 +13,20 @@ class dbHandler():
         self.cur.close()
         self.conn.close()
 
-    def ex(self, query, param=None):
+    def ex(self, query, param=None, retry=False):
         try:
             if not param:
                 self.cur.execute(query)
             else:
                 self.cur.execute(query, param)
         except Exception as exc:
-            self.conn.commit()
             print(exc)
-            return
-        try:
+            if not retry:
+                try:
+                    self.conn = psycopg2.connect(**self.params)
+                    return self.ex(query, param, True)
+                except psycopg2.errors as exc:
+                    print(exc)
+        finally:
             self.conn.commit()
             return self.cur.fetchall()
-        except:
-            pass
